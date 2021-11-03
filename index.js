16db52988217e3f041f86d7f215d8e22802995d3
MoisesMJr <moisesmanonjr@hotmail.com>
2021-11-03 15:30:00 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var p1Score = 0;
+var p2Score = 0;
+var tie = 0;
+var turn = 1;
+
+function checkWinX(){
+    if ($(".row-1 .box.x").length === 3
+    || $(".row-2 .box.x").length === 3
+    || $(".row-3 .box.x").length === 3
+    || $(".col-1.x").length === 3
+    || $(".col-2.x").length === 3
+    || $(".col-3.x").length === 3
+    || $("#1").hasClass("x")
+    && $("#5").hasClass("x")
+    && $("#9").hasClass("x")
+    || $("#3").hasClass("x")
+    && $("#5").hasClass("x")
+    && $("#7").hasClass("x")){
+        console.log("Working")
+    }
+}
+function checkWinO(){
+    if ($(".row-1 .box.o").length === 3
+    || $(".row-2 .box.o").length === 3
+    || $(".row-3 .box.o").length === 3
+    || $(".col-1.o").length === 3
+    || $(".col-2.o").length === 3
+    || $(".col-3.o").length === 3
+    || $("#1").hasClass("o")
+    && $("#5").hasClass("o")
+    && $("#9").hasClass("o")
+    || $("#3").hasClass("o")
+    && $("#5").hasClass("o")
+    && $("#7").hasClass("o")){
+        console.log("Working")
+    }
+}
+
+$(".box").click(function(){
+    if (turn === 1){
+        $(this).text("x")
+        $(this).addClass("x")
+        turn = 2;
+        $("#turn").text(2)
+        checkWinX();
+    }
+    else
+    {
+        $(this).text("o")
+        $(this).addClass("o")
+        turn = 1;
+        $("#turn").text(1)
+        checkWinO()
+    }
+})
